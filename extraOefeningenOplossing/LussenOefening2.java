1f579338fff85cb1c2ef22f4e9ff4e514ef48716
93design <goegebeurjamie@gmail.com>
2019-02-05 21:37:48 +0100

    third commit  - adding solutions to axtra challenges chapter 4,5 and 6

---
+++ extraOefeningenOplossing/LussenOefening2.java
@@ -0,0 +1,18 @@
+package Oplossing;
+
+public class LussenOefening2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		int i = 1;
+		do {
+			if(!(i%2 ==0)) {
+				System.out.println(i);
+			}
+			i++;
+		} while(i<100);
+	}
+
+}
---
+++ extraOefeningenOplossing/LussenOefening3.java
@@ -0,0 +1,15 @@
+package Oplossing;
+
+public class LussenOefening3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub		
+		int i = 1;
+		do {
+			if(!(i%2 ==0)) {
+				System.out.println(i);
+			}
+			i++;
+		} while(i<100);
+	}
+}
