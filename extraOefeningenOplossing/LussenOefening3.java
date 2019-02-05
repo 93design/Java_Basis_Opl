d25a04d08525661a6f190cb518c887ed2107c84b
93design <goegebeurjamie@gmail.com>
2019-02-05 21:41:41 +0100

    fourth commit - switched challenges names

--- extraOefeningenOplossing/LussenOefening3.java
+++ extraOefeningenOplossing/LussenOefening3.java
@@ -1,15 +1,29 @@
 package Oplossing;
 
+import java.util.Scanner;
+
 public class LussenOefening3 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub		
-		int i = 1;
-		do {
-			if(!(i%2 ==0)) {
-				System.out.println(i);
+				Scanner input = new Scanner(System.in);
+				double brutoLoon = input.nextDouble();
+				double nettoLoon = 0;
+				
+				final double HOGE_SCHIJF= 38;
+				final double MIDDEL_SCHIJF= 35;
+				final double LAGE_SCHIJF= 27;
+				
+				
+				if(brutoLoon > 3000) {
+					nettoLoon = brutoLoon *(HOGE_SCHIJF/100);
+				} else if (brutoLoon <= 3000 &&brutoLoon >=2000) {
+					nettoLoon = brutoLoon *(MIDDEL_SCHIJF/100);
+				} else if(brutoLoon <2000) {
+					nettoLoon = brutoLoon *(LAGE_SCHIJF/100);
+				}
+				
+				System.out.println("uw nettoLoon is" + nettoLoon);
+			
 			}
-			i++;
-		} while(i<100);
-	}
 }
