33b7c1e73e4cb199560009ba8dad7563e23b2343
93design <goegebeurjamie@gmail.com>
2019-03-18 13:00:24 +0100

    random main toevoegen

---
+++ extraOefeningenOplossing/javadocs/garageDock/GarageAppSmallRandom.java
@@ -0,0 +1,34 @@
+package app;
+
+import garage.Color;
+import garage.FamilyCar;
+import garage.Garage;
+import garage.GarageFullException;
+import garage.SportCar;
+import garage.VehicleAlreadyInGarageException;
+
+public class GarageAppSmallRandom {
+
+	public static void main(String[] args) {
+		Garage garage = new Garage();
+		FamilyCar familyCar = new FamilyCar(Color.BLUE);
+		SportCar sportCar = new SportCar(Color.WHITE);
+		familyCar.accelerate(5.0);
+
+		try {
+			garage.addVehicle(familyCar);
+			garage.addVehicle(sportCar); 
+		} catch (GarageFullException e) {
+			System.out.println();
+			System.out.println(e.getMessage());
+		} catch (VehicleAlreadyInGarageException e) {
+			System.out.println(e.getMessage());
+		}
+		
+		System.out.println("garage Size = " + garage.getSize());
+		garage.clear();
+		System.out.println("garage Size = " + garage.getSize());
+	}
+
+
+}
